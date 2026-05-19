69c456a1cb7fb826b25499a97793ed0254a4471c
ranjeetk77080-commits <ranjeetk77080@gmail.com>
2026-05-19 23:25:41 +0530

    rename package name from com.difmo.difwa to com.difmo.milkdi on Android and iOS

--- milkdi_continue_flutter_app/android/app/build.gradle.kts
+++ milkdi_continue_flutter_app/android/app/build.gradle.kts
@@ -19,7 +19,7 @@ if (localPropertiesFile.exists()) {
 val mapsApiKey: String = localProperties.getProperty("MAPS_API_KEY") ?: ""
 
 android {
-    namespace = "com.difmo.difwa"
+    namespace = "com.difmo.milkdi"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -35,7 +35,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.difmo.difwa"
+        applicationId = "com.difmo.milkdi"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
--- milkdi_continue_flutter_app/android/app/src/main/kotlin/com/difmo/milkdi/MainActivity.kt
+++ milkdi_continue_flutter_app/android/app/src/main/kotlin/com/difmo/milkdi/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.difmo.difwa
+package com.difmo.milkdi
 
 import io.flutter.embedding.android.FlutterActivity
 
